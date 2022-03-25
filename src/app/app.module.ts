1602a3275ea1bb3940f97838759bf0301f260aea
Psydbone <d.scorcia@studenti.poliba.it>
2022-03-25 08:47:25 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { HelloComponent } from './hello.component';
 import { TestModule } from './test.module';
 import { TestService, TestService2 } from './test.service';
 import { TestDirective } from './test.directive';
+import { arrayDirettiva } from './array.directive';
+import { TableComponent } from './table.component';
+import { ArrayService } from './array.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,TestModule],
-  declarations: [ AppComponent, HelloComponent, TestDirective],
-  providers : [TestService,TestService2],
+  declarations: [ AppComponent, HelloComponent, TestDirective,arrayDirettiva,TableComponent],
+  providers : [TestService,TestService2,ArrayService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
